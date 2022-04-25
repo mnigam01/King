9ee77ca3f7573fb8af686b6091c81fb618287897
mnigam01 <mridulkumar1998@gmail.com>
2022-04-25 12:06:32 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,104 @@
+//jshint esversion:6
+
+require('dotenv').config()
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+const _ = require("lodash");
+const ejs = require("ejs");
+const encrypt = require("mongoose-encryption");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+
+
+
+mongoose.connect("mongodb://localhost:27017/userDB");
+
+const userSchema = new mongoose.Schema(
+    {
+        email:String,
+        password:String
+    }
+);
+
+console.log(process.env.SECRET);
+
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password']});
+
+const User = mongoose.model("User",userSchema);
+
+
+
+
+app.get("/",function(req,res)
+{
+    res.render("home");
+});
+
+
+app.get("/login",function(req,res)
+{
+    res.render("login");
+});
+
+app.get("/register",function(req,res)
+{
+    res.render("register");
+});
+
+
+app.post("/register",function(req,res)
+{
+    const newUser = new User({
+        email:req.body.username,
+        password:req.body.password
+    });
+    //console.log(newUser);
+    newUser.save(function(err)
+    {
+        if(!err)
+        {
+            res.render("secrets");
+        }else{
+            console.log(err);
+        }
+    });
+});
+
+
+app.post("/login",function(req,res)
+{
+    const loginEmail = req.body.username;
+    User.findOne({email:loginEmail},function(err,foundUser)
+    {
+        if (!err)
+        {
+            if (foundUser)  // this value can be Null also in case we didn't find any document
+            {
+                if (foundUser.password==req.body.password)
+                {
+                    console.log(foundUser.password);
+                    res.render("secrets");
+                }
+                else{
+                    console.log("Unauthorized access");
+                }
+            }
+        }
+        else{
+            console.log(err);
+        }
+    })
+});
+
+
+
+
+
+app.listen(process.env.PORT || 3000,function()
+{
+    console.log("Successfully connected to port!");
+})
